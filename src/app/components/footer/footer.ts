03436a0a52d72e14bd078ff51f2274b88c13dfc7
Gaurav Jamdade <jamdadegaurav01@gmail.com>
2026-08-07 03:49:38 +0530

    Datetime_fix

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // ✅ IMPORTANT
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // ✅ Add this
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
-export class Footer {
+export class Footer implements OnInit {
 
   currentYear = new Date().getFullYear();
+  currentDateTime: Date = new Date();
 
+  ngOnInit() {
+    setInterval(() => {
+      this.currentDateTime = new Date();
+    }, 1000);
+  }
 }
