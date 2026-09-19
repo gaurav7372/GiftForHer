f68d36781dc7085e1a6fdcc126a02542f4c24151
Gaurav Jamdade <jamdadegaurav01@gmail.com>
2026-09-20 01:06:30 +0530

    Fix Navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,31 +1,56 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-declare var bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
 
-  closeNavbar() {
+  scrollTo(id: string): void {
 
-    const navbar = document.getElementById('navbarMenu');
+    const element = document.getElementById(id);
 
-    if (navbar && navbar.classList.contains('show')) {
+    if (!element) {
+      return;
+    }
 
-      const collapse =
-        bootstrap.Collapse.getInstance(navbar) ||
-        new bootstrap.Collapse(navbar, {
-          toggle: false
-        });
+    const navbarHeight = 80;
 
-      collapse.hide();
+    const y =
+      element.getBoundingClientRect().top +
+      window.scrollY -
+      navbarHeight;
 
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth'
+    });
+  }
+
+
+  closeNavbar(): void {
+
+    const navbarMenu =
+      document.getElementById('navbarMenu');
+
+    if (!navbarMenu) {
+      return;
+    }
+
+    // Close Bootstrap mobile menu
+    navbarMenu.classList.remove('show');
+
+    const toggler =
+      document.querySelector('.navbar-toggler');
+
+    if (toggler) {
+      toggler.setAttribute(
+        'aria-expanded',
+        'false'
+      );
     }
 
   }
